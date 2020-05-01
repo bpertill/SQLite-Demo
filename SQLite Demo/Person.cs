25765c6725692e3523c1e436f0fad6e0a5d48f30
Bernhard Pertiller <bernhard.pertiller91@gmail.com>
2020-05-01 18:49:14 +0200

    Add project files.

---
+++ SQLite Demo/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLite_Demo
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}: {FirstName} {LastName} {DoB}";
+        }
+    }
+}
---
+++ SQLite Demo/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Data.SQLite;
+
+namespace SQLite_Demo
+{
+    class Program
+    {
+        public static string Connectionstring = "Data Source=.\\DemoSQLiteDb.db ";
+        static void Main(string[] args)
+        {
+
+            foreach (var person in GetPeople())
+            {
+                Console.WriteLine(person.ToString());
+            }
+            Console.ReadLine();
+
+        }
+
+        private static int AddPerson(Person person)
+        {
+            var query = "INSERT INTO Person(FirstName,LastName, DoB) VALUES (@firstName, @lastName, @dob)";
+            var args = new Dictionary<string, object>
+            {
+                {"@firstName",person.FirstName },
+                {"@lastName",person.LastName },
+                {"@dob",person.DoB }
+            };
+            return ExecuteWrite(query, args);
+        }
+
+        private static Person GetPersonById(int id)
+        {
+            var query = "SELECT Id,FirstName, LastName, DoB FROM Person WHERE Id = @id";
+
+            var args = new Dictionary<string, object>
+            {
+                {"@id",id }
+            };
+
+            DataTable dt = ExecuteRead(query, args);
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            var person = new Person
+            {
+                Id = Convert.ToInt32(dt.Rows[0]["Id"]),
+                FirstName = Convert.ToString(dt.Rows[0]["FirstName"]),
+                LastName = Convert.ToString(dt.Rows[0]["LastName"]),
+                DoB = Convert.ToDateTime(dt.Rows[0]["DoB"])
+            };
+
+            return person;
+        }
+
+        private static List<Person> GetPeople()
+        {
+            var query = "SELECT Id,FirstName, LastName, DoB FROM Person";
+            DataTable dt = ExecuteRead(query);
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                people.Add(
+                    new Person
+                    {
+                        Id = Convert.ToInt32(dt.Rows[i]["Id"]),
+                        FirstName = Convert.ToString(dt.Rows[i]["FirstName"]),
+                        LastName = Convert.ToString(dt.Rows[i]["LastName"]),
+                        DoB = Convert.ToDateTime(dt.Rows[i]["DoB"])
+                    });
+            }
+            return people;
+        }
+
+        private static DataTable ExecuteRead(string query, Dictionary<string, object> args = null)
+        {
+            if (string.IsNullOrEmpty(query.Trim()))
+                return null;
+
+            using var con = new SQLiteConnection(Connectionstring);
+            con.Open();
+
+            using var cmd = CreateCommandWithParameters(query, con, args);
+
+            using var da = new SQLiteDataAdapter(cmd);
+            var dt = new DataTable();
+            da.Fill(dt);
+            da.Dispose();
+            return dt;
+        }
+
+        private static int ExecuteWrite(string query, Dictionary<string, object> args = null)
+        {
+            using var con = new SQLiteConnection(Connectionstring);
+            con.Open();
+
+            using var cmd = CreateCommandWithParameters(query, con, args);
+
+            return cmd.ExecuteNonQuery();
+        }
+
+        private static SQLiteCommand CreateCommandWithParameters(string query, SQLiteConnection con, Dictionary<string, object> args = null)
+        {
+            var cmd = new SQLiteCommand(query, con);
+            if (args != null)
+            {
+                foreach (KeyValuePair<string, object> entry in args)
+                {
+                    cmd.Parameters.AddWithValue(entry.Key, entry.Value);
+                }
+            }
+            return cmd;
+        }
+    }
+}
