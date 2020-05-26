a01fcf88bb3966100b7c0f57692a9e51fe7030ae
Bernhard Pertiller <s1013195@stud.sbg.ac.at>
2020-05-26 21:44:17 +0200

    Added API Projekt

---
+++ API/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SQLiteLibrary;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Person Get()
+        {
+            return  new Person { Id = 1, DoB = new DateTime(1991, 04, 18), FirstName = "myfirstname", LastName = "mylastname" } ;
+        }
+    }
+}
